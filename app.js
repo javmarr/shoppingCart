cb8451407fb14998438c6141b4e3ba2a46bde044
javmarr <j_marroquin@live.com>
2016-11-19 12:19:17 -0600

    added bool to run certain code when using heroku only (should allow for deployment on openshift and heroku

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const MONGO_PASSWORD = process.env.OPENSHIFT_MONGODB_DB_PASSWORD;
 const DB_NAME = 'shoppingcart';
 
 const HEROKU_MLAB_URI = process.env.HEROKU_MLAB_URI;
+const USE_HEROKU = process.env.USE_HEROKU;
 
 // use mlab for db regardless where it's hosted
 if(MONGO_HOST) {
@@ -45,8 +46,8 @@ else {
 
 
 var app = express();
-// if(HEROKU_MLAB_URI && !MONGO_HOST)
-  // app.set('port', (process.env.PORT || 5000));
+if(USE_HEROKU)
+  app.set('port', (process.env.PORT || 5000));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -151,10 +152,10 @@ app.use(function(err, req, res, next) {
   });
 });
 
-// if(HEROKU_MLAB_URI && !MONGO_HOST)
-//   app.listen(app.get('port'), function() {
-//     console.log('Node app is running on port', app.get('port'));
-//   });
+if(USE_HEROKU)
+  app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+  });
 
 
 module.exports = app;
